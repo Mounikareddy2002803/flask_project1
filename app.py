e7401c50f435f8597f57a84870e0c74f80a18a2c
Mounikareddy2002803 <mounikareddy2002803@gmail.com>
2023-02-28 11:43:06 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+# create Application instance
+# Application instance is an object of Flask Class
+# so import Flask class
+# Flask Class will take an argument that is Name of the current module
+# standard way to represent current module name is __name__
+
+from flask import Flask
+
+AI=Flask(__name__)
+
+# create one view function and decoorate it with routemethod of AI
+
+@AI.route('/wish')
+def wish():
+    return 'This is my first flask view function'
+@AI.route('/second')
+def second():
+    return 'Second view function'
+
+if __name__=='__main__':
+    AI.run(debug=True)
